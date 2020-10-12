cb26690d3e614a3a992547eb6a9dd9e077e46b09
genshen <genshen@users.noreply.github.com>
2020-10-12 09:53:32 +0000

    deploy: ed743dd6630d1d132d7ac7d6715a5dff93851b8e

---
+++ precache-manifest.49b67064ee457a76e644cb7b4204d5d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da4e39169d15f70356037bb2f3230bf",
+    "url": "/misa-md-users/index.html"
+  },
+  {
+    "revision": "106ffac1fa97708db4ad",
+    "url": "/misa-md-users/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "6ea8bd8e17a2ca7942a0",
+    "url": "/misa-md-users/static/css/main.f6698ac8.chunk.css"
+  },
+  {
+    "revision": "106ffac1fa97708db4ad",
+    "url": "/misa-md-users/static/js/2.c62a68ad.chunk.js"
+  },
+  {
+    "revision": "5eb8a1f43a1a98f94fd11dc351ab6fb6",
+    "url": "/misa-md-users/static/js/2.c62a68ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea8bd8e17a2ca7942a0",
+    "url": "/misa-md-users/static/js/main.8855fffb.chunk.js"
+  },
+  {
+    "revision": "21e7cdf939a3c9de3a02",
+    "url": "/misa-md-users/static/js/runtime-main.2eae0873.js"
+  }
+]);
